59a38fb33ee3de540bcf107db9a6f99cfa03bf12
PANDIYA RAJAN G <146184670+pandygl2907@users.noreply.github.com>
2023-10-06 11:44:03 +0530

    add2nos1.c

--- add2nos1.c
+++ add2nos1.c
@@ -1,14 +1,11 @@
 // C Program to add 2 numbers (For constant input)
-#include<stdio.h>
-#include<conio.h>
-
-int main()
+#include<stdio.h>            // Header File
+int main()                   // Main Function Starts
 {
-  int a=10;
-  int b=15;
-  int c;
-  c = a + b;
-  print("C is : %d", c);
-  getch();
+  int a=10;                  // variable Declaration and Initialization
+  int b=15;                  // variable Declaration and Initialization     
+  int c;                     // variable Declaration
+  c = a + b;                 // Calculation Part  
+  printf("C is : %d", c);    // Main Function Ends
   return 0;  
 }
