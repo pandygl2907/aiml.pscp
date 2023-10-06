6a71867c2ec9555733a9e6875ae0330c0d5bc94f
PANDIYA RAJAN G <146184670+pandygl2907@users.noreply.github.com>
2023-10-06 11:49:06 +0530

    Create add2nos2.c

---
+++ add2nos2.c
@@ -0,0 +1,11 @@
+// C Program to add 2 numbers (Get input from user)
+#include<stdio.h>                        // Header File
+int main()                               // Main Function
+{                                         // Main Function Starts
+  int a, b, c;                           // variable Declaration
+  printf("Enter 2 nos. to add : \n");    // Print the Result to the User
+  scanf("%d %d", &a, &b);                // Get input from User
+  c = a + b;                             // Calculation Part  
+  printf("The Result is : %d", c);       // Print the Result to the User
+  return 0;                              // Return the result through an integer
+}                                        // Main Function Ends
