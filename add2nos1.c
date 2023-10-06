dd5bab5b880e0701805a16e1193614153c64712c
PANDIYA RAJAN G <146184670+pandygl2907@users.noreply.github.com>
2023-10-06 11:51:21 +0530

    C Program to add 2 numbers (For constant input)

--- add2nos1.c
+++ add2nos1.c
@@ -1,11 +1,11 @@
 // C Program to add 2 numbers (For constant input)
 #include<stdio.h>            // Header File
-int main()                   // Main Function Starts
-{
+int main()                   // Main Function 
+{                            // Main Function Starts
   int a=10;                  // variable Declaration and Initialization
   int b=15;                  // variable Declaration and Initialization     
   int c;                     // variable Declaration
   c = a + b;                 // Calculation Part  
-  printf("C is : %d", c);    // Main Function Ends
-  return 0;  
+  printf("C is : %d", c);    // Return the result through an integer
+  return 0;                  // Main Function Ends
 }
