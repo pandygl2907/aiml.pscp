485e9e651b653c50d96b6fd9f68276ee4ca113b8
PANDIYA RAJAN G <146184670+pandygl2907@users.noreply.github.com>
2023-11-07 13:06:26 +0530

    C Program to Check the given number is Prime number

---
+++ primeno.c
@@ -0,0 +1,36 @@
+// C Program to Check the given number is Prime number...
+
+#include<stdio.h>
+int main()    
+{    
+    int n, i, temp=0;
+    
+    printf("Enter any number : ");    
+    scanf("%d", &n);    
+    
+    if(i==0 || i==1)
+    {
+        temp = 1;
+    }
+    for(i=2; i<n/2; i++)
+    {
+        if(n%i == 0)
+        {
+            temp = 1;
+            break;
+        }
+    }
+    
+    if(temp == 0)
+    {
+        printf("It's prime number...");
+    }
+    
+    else
+    {
+        printf("It's not a prime number...");
+    }
+    
+    printf("\nEnd of Program...");
+    return 0;  
+}
